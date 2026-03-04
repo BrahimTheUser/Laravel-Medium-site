658c2256e24c3e7135eccbc27e60e7d89932d547
Aouadi Brahim <aouadib86@gmail.com>
2026-03-04 17:33:33 +0000

    implementing public user profile page

--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,29 @@
 
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\PublicProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', [PostController::class, 'index'])
-->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/@{user:username}', [PublicProfileController::class, 'show'])
+    ->name('profile.show');
+
+Route::middleware(['auth', 'verified'])->group(function() {
+    Route::get('/', [PostController::class, 'index'])
+        ->name('dashboard');
+
+    Route::get('/post/create', [PostController::class, 'create'])
+        ->name('post.create');
+
+    Route::post('/post/create', [PostController::class, 'store'])
+        ->name('post.store');
+
+    Route::get('/@{username}/{post:slug}', [PostController::class, 'show'])
+        ->name('post.show');
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -17,4 +32,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
